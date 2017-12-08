76c5610adfea08b3aa6e2296c97251bb3da7ff5e
joco25 <onungwej@gmail.com>
2017-12-08 06:20:27 +0100

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.get('/todos/:id',(req,res)=>{
 	}
 	Todo.findById(id).then((todo)=>{
 		if(!todo){
-			return res.send({error:"No todo Found"})
+			return res.status(404).send({error:"No todo Found"})
 		}
 		res.send({todo})
 	})
