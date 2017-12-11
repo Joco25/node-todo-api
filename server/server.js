c1d0f86274b5e1f1258ae72db9573277c59f30bc
joco25 <onungwej@gmail.com>
2017-12-11 17:00:12 +0100

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {ObjectID} = require('mongodb')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todos')
 var {User}= require('./models/user')
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express()
 const port = process.env.PORT
@@ -95,6 +96,12 @@ app.post('/users',(req,res)=>{
 	}).then((token)=>{
 		res.header('x-auth',token).send(user)
 	}).catch((e)=>res.status(400).send(e))
+})
+
+
+
+app.get('/users/me',authenticate, (req,res)=>{
+	res.send(req.user)
 })
 
 app.listen(port,()=>{
